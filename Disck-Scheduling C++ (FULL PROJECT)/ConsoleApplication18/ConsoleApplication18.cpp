b6e9a5b50bc274f1efc0b53423977c181a193fbd
xTrimy <Mohamed.ashraf881999@gmail.com>
2020-12-29 21:16:15 +0200

    SSTF algorithm, created a function for each algorithm

--- Disck-Scheduling C++ (FULL PROJECT)/ConsoleApplication18/ConsoleApplication18.cpp
+++ Disck-Scheduling C++ (FULL PROJECT)/ConsoleApplication18/ConsoleApplication18.cpp
@@ -9,8 +9,14 @@
 #include <iterator> 
 #include <string>
 using namespace std;
+
+void sstf( int*, int, int, int, int);
+void fcfs(int *, int, int, int, int, bool);
+void scan(int *, int, int, int, int, bool);
+
 int main()
 {
+
 	//INPUTS
 	cout << "enter the head" << endl;
 	int head;
@@ -33,35 +39,102 @@ int main()
 	bool upper = false;
 	cout << "1-right" << " | " << "0-left" << endl;
 	cin >> upper;
+	
+	bool cont = 1;
+	while (cont != 0) {
 
-	int total_distance = 0;
 
-	//FCFS
-	cout << "FCFS: " << endl;
-	total_distance = abs(head - requests[0]);
-	for (int i = 0; i < requests_count - 1; i++){
-		//cout << requests[i] << " - " << requests[i+1] << " = " << abs(requests[i] - requests[i + 1]) << endl;
-		total_distance += abs(requests[i] - requests[i + 1]);
+		////////////////
+		int algorithm;
+		cout << "Choose an algorithm\n";
+		cout << "1 - FCFS\n";
+		cout << "2 - SCAN\n";
+		cout << "3 - SSTF\n";
+
+		cout << endl;
+		cout << ": ";
+		cin >> algorithm;
+
+		switch (algorithm) {
+		case 1:
+			fcfs(requests, head, requests_count, seek_time, end, upper);
+			break;
+		case 2:
+			scan(requests, head, requests_count, seek_time, end, upper);
+			break;
+		case 3:
+			sstf(requests, head, requests_count, seek_time, end);
+			break;
+		dafault:
+			cout << "This is not a valid option, please try again";
+		}
+
+		cout << "\n\nType 1 to continue or 0 to exit: ";
+		cin >> cont;
 	}
+}
+///////////////////////
+///////////////////////
 
-	cout << "Order: " << head << " -> ";
+//SSTF
+void sstf(int *requests_,int head,int requests_count,int seek_time, int end) {
+	cout << "SSTF: " << endl;
+	int * requests = new int[requests_count];
+	for (int i = 0; i < requests_count; i++) {
+		requests[i] = requests_[i];
+	}
+	int*sstf_requests = new int[requests_count];
+	for (int i = 0; i < requests_count; i++) {
+		sstf_requests[i] = requests[i];
+	}
+	int current_request = head;
+	int current_request_index = -1;
+	int least_distance = 99999;
+	int least_distance_index = 0;
+	for (int i = 0; i < requests_count; i++) {
+		for (int j = 0; j < requests_count; j++) {
+			if (j == current_request_index) {
+				continue;
+			}
+			if (least_distance > abs(current_request - requests[j]) ) {
+				least_distance = abs(current_request - requests[j]);
+				least_distance_index = j;
+			}
+		}
+		cout << endl << endl;
+		least_distance = 99999;
+		sstf_requests[i] = requests[least_distance_index];
+		current_request = requests[least_distance_index];
+		requests[least_distance_index] = 99999;
+	}
+	
 	for (int i = 0; i < requests_count; i++) {
 		string arrow = " -> ";
 		if (i == requests_count - 1) {
 			arrow = "";
 		}
-		cout << requests[i] << arrow;
+		cout << sstf_requests[i] << arrow;
 	}
-    cout << "\n" << "Seek: " << total_distance << " cylinders, " << total_distance*seek_time << "ms";
+
+	int total_distance = abs(head - sstf_requests[0]);
+	for (int i = 0; i < requests_count - 1; i++) {
+		total_distance += abs(sstf_requests[i] - sstf_requests[i + 1]);
+	}
+	cout << "\n" << "Seek: " << total_distance << " cylinders, " << total_distance * seek_time << "ms";
 	cout << endl << endl;
+}
+///////////////////////
+///////////////////////
 
-	////////////////////////////
-	////////////////////////////
-
-	//SCAN
+//SCAN
+void scan(int *requests_, int head, int requests_count, int seek_time,int end, bool upper) {
 	cout << "Scan: " << endl;
+	int * requests = new int[requests_count];
+	for (int i = 0; i < requests_count; i++) {
+		requests[i] = requests_[i];
+	}
 	sort(requests, requests + requests_count);
-
+	
 	list<int>left;
 	list<int>right;
 	for (int i = 0; i < requests_count; i++)
@@ -72,7 +145,7 @@ int main()
 			left.push_back(requests[i]);
 	}
 	list<int> SCAN_requests;
-	SCAN_requests.push_back (head);
+	SCAN_requests.push_back(head);
 	for (int i = 0; i < 2; i++)
 	{
 		if (upper)
@@ -83,7 +156,7 @@ int main()
 			}
 
 		}
-		else 
+		else
 		{
 			for (auto i = left.rbegin(); i != (left.rend()); i++)
 			{
@@ -96,17 +169,41 @@ int main()
 		}
 		upper = !upper;
 	}
-	
-	
-	total_distance = 0;
+
+
+	int total_distance = 0;
 	cout << "Order: ";
 	for (list <int> ::iterator i = SCAN_requests.begin(); i != prev(SCAN_requests.end()); i++) {
-		cout << *i << " -> " ;
+		cout << *i << " -> ";
 		//cout << *i << " - " << *next(i) << " = " << abs(*i - *next(i)) << endl;
 		total_distance += abs(*i - *next(i));
 
 	}
 	cout << *prev(SCAN_requests.end());
+	cout << "\n" << "Seek: " << total_distance << " cylinders, " << total_distance * seek_time << "ms";
+	cout << endl << endl;
+
+}
+////////////////////////
+///////////////////////
+
+//FCFS
+void fcfs(int *requests, int head, int requests_count, int seek_time, int end, bool upper) {
+	cout << "FCFS: " << endl;
+	int total_distance = abs(head - requests[0]);
+	for (int i = 0; i < requests_count - 1; i++) {
+		//cout << requests[i] << " - " << requests[i+1] << " = " << abs(requests[i] - requests[i + 1]) << endl;
+		total_distance += abs(requests[i] - requests[i + 1]);
+	}
+
+	cout << "Order: " << head << " -> ";
+	for (int i = 0; i < requests_count; i++) {
+		string arrow = " -> ";
+		if (i == requests_count - 1) {
+			arrow = "";
+		}
+		cout << requests[i] << arrow;
+	}
 	cout << "\n" << "Seek: " << total_distance << " cylinders, " << total_distance * seek_time << "ms";
 	cout << endl << endl;
 }
